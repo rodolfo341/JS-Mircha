f8b2396c541db1e0cf900f0d142a9cdd452271e0
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-04-29 11:21:30 -0400

    .

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 
 let frase = prompt("ingrese frase");
-let cont = 0;
+let contA = 0;
+let contE = 0;
+let contI = 0;
+let contO = 0;
+let contU = 0;
 let cadena = "";
 
 for (let i = 0; i < frase.length; i++) {
-    if(    frase.charAt(i) === "a" 
-        || frase.charAt(i) === "e"
-        || frase.charAt(i) === "i"
-        || frase.charAt(i) === "o"
-        || frase.charAt(i) === "u"
-        || frase.charAt(i) === "A"
-        || frase.charAt(i) === "E"
-        || frase.charAt(i) === "I"
-        || frase.charAt(i) === "O"
-        || frase.charAt(i) === "U"
-         ){
-        cadena = cadena + frase.charAt(i);
+    if( frase.charAt(i) === "a" ) {
+        contA++;
+    }else if( frase.charAt(i) === "e" ) {
+        contE++;
+    }else if( frase.charAt(i) === "i" ) {
+        contI++;
+    }else if( frase.charAt(i) === "o" ) {
+        contO++;
+    }else if( frase.charAt(i) === "u" ) {
+        contU++;
     }    
-    
 } 
-document.getElementById("parrafo1").innerHTML = cadena;
+document.getElementById("parrafoA").innerHTML = "Hay " + contA + " letras 'a' ";
+document.getElementById("parrafoE").innerHTML = "Hay " + contE + " letras 'e' ";
+document.getElementById("parrafoI").innerHTML = "Hay " + contI + " letras 'i' ";
+document.getElementById("parrafoO").innerHTML = "Hay " + contO + " letras 'o' ";
+document.getElementById("parrafoU").innerHTML = "Hay " + contU + " letras 'u' ";
 
