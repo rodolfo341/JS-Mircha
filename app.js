7f8f824cf46b2e47a3cfc88d57f68d3cc2296186
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-04-29 14:51:06 -0400

    .

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
 
-let frase = prompt("ingrese frase");
-let contA = 0;
-let contE = 0;
-let contI = 0;
-let contO = 0;
-let contU = 0;
-let cadena = "";
 
-for (let i = 0; i < frase.length; i++) {
-    if( frase.charAt(i) === "a" ) {
-        contA++;
-    }else if( frase.charAt(i) === "e" ) {
-        contE++;
-    }else if( frase.charAt(i) === "i" ) {
-        contI++;
-    }else if( frase.charAt(i) === "o" ) {
-        contO++;
-    }else if( frase.charAt(i) === "u" ) {
-        contU++;
-    }    
-} 
-document.getElementById("parrafoA").innerHTML = "Hay " + contA + " letras 'a' ";
-document.getElementById("parrafoE").innerHTML = "Hay " + contE + " letras 'e' ";
-document.getElementById("parrafoI").innerHTML = "Hay " + contI + " letras 'i' ";
-document.getElementById("parrafoO").innerHTML = "Hay " + contO + " letras 'o' ";
-document.getElementById("parrafoU").innerHTML = "Hay " + contU + " letras 'u' ";
+do {
+    
+    let num = parseInt(prompt("Ingrese numero a comprobar"));
+    let numDiv = parseInt(prompt("ingrese numero 2, 3, 5 o 7"));
+
+    if ( num % numDiv == 0){
+        document.getElementById("parrafo").innerHTML = "El numero " + num + " es divisible por " + numDiv;
+    }else{
+        document.getElementById("parrafo").innerHTML = "El numero " + num + " no es divisible por " + numDiv;
+    }
+
+} while (!( numDiv == 2 || numDiv == 3 || numDiv == 5 || numDiv == 7 ));
 
