b9426ed40358041a6218b88d4f7034cfe33f608a
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-04-29 10:17:57 -0400

    .

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 
 let numero1 = parseInt(prompt("ingrese numero 1"));
 let numero2 = parseInt(prompt("ingrese numero 2"));
+let numero3 = parseInt(prompt("ingrese numero 2"));
 
-if ( numero1 > numero2 ) document.getElementById("parrafo1").innerHTML = numero1 + " > " + numero2; 
-else if ( numero2 > numero1 ) document.getElementById("parrafo1").innerHTML = numero2 + " > " + numero1; 
-else document.getElementById("parrafo1").innerHTML = "Son iguales"; 
+if ( numero1 > numero2 && numero1 > numero3 ) document.getElementById("parrafo1").innerHTML = "el mayor es " + numero1;
+
+else if ( numero2 > numero1 && numero2 > numero3 ) document.getElementById("parrafo1").innerHTML = "el mayor es " + numero2;
+
+else document.getElementById("parrafo1").innerHTML = "el mayor es " + numero3; 
