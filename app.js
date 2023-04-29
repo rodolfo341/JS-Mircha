5613094945ff922569b5a41673bad8ddcd587113
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-04-29 10:12:59 -0400

    .

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 
-let nombreUser = prompt("ingrese su nombre");
-document.getElementById("parrafo1").innerHTML = "Hola " + nombreUser; 
+let numero1 = parseInt(prompt("ingrese numero 1"));
+let numero2 = parseInt(prompt("ingrese numero 2"));
+
+if ( numero1 > numero2 ) document.getElementById("parrafo1").innerHTML = numero1 + " > " + numero2; 
+else if ( numero2 > numero1 ) document.getElementById("parrafo1").innerHTML = numero2 + " > " + numero1; 
+else document.getElementById("parrafo1").innerHTML = "Son iguales"; 
