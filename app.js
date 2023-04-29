b719cfe87a1b600be189f5f6e474ea33ba26fae8
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-04-29 10:42:48 -0400

    .

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 
 let frase = prompt("ingrese un numero");
-let letra = "a";
 let cont = 0;
 
 for (let i = 0; i < frase.length; i++) {
-    if(frase.charAt(i) === letra){
+    if(    frase.charAt(i) === "a" 
+        || frase.charAt(i) === "e"
+        || frase.charAt(i) === "i"
+        || frase.charAt(i) === "o"
+        || frase.charAt(i) === "u"
+        || frase.charAt(i) === "A"
+        || frase.charAt(i) === "E"
+        || frase.charAt(i) === "I"
+        || frase.charAt(i) === "O"
+        || frase.charAt(i) === "U"
+         ){
         cont++;
     }    
 } 
 
 
-document.getElementById("parrafo1").innerHTML = "La letra " + letra + " se repite " + cont + " veces";
+document.getElementById("parrafo1").innerHTML = "Total de vocales es " + cont;
 
 
